e3b8d55a7b7e261a7d41cb28e3ec12763c11cedc
Rino André Johnsen <rinoandrejohnsen@gmail.com>
2012-12-07 13:43:42 +0100

    Added new data to the tickerModule

--- source/modules/tickerModule/data/data.js
+++ source/modules/tickerModule/data/data.js
@@ -21,20 +21,47 @@ define({
         {
             id: 3,
             type: {
-                id: "Cujo",
+                id: "Curl",
                 icon: "source/modules/twitterModule/resources/icons/twitter.png",
                 color: "#3CBAFF"
             },
-            text: "cujo’s extensible architecture and use of industry standards allows it to work seamlessly with your current application framework -- and adapt easily to your next. You focus on the business logic of your application components. cujo loads and assembles your components and then wires them all together."
+            text: "curl.js is a small and very fast AMD-compliant asynchronous loader. Size: 5KB (2.5KB gzipped) using Google's Closure Compiler. If you'd like to use curl.js for non-AMD modules (ordinary javascript files), you'll want to use a version with the js! plugin built in. You may also want to build-in the domReady module. The combined curl+js+domReady loader is still only 6.5KB (3KB gzipped)."
         },
         {
             id: 4,
             type: {
-                id: "Wire",
+                id: "Cola",
                 icon: "source/modules/facebookModule/resources/icons/facebook.jpg",
                 color: "#3b5b96"
             },
-            text: "Wire.js requires an AMD compatible loader and an ES5 environment when running in a browser. If you aren't already using an AMD loader and ES5 shim, we recommend curl and poly. Other AMD loaders, such as RequireJS, and ES5 shims, such as es5-shim, should work as well."
+            text: "Cujo object linker"
+        },
+        {
+            id: 5,
+            type: {
+                id: "Poly",
+                icon: "source/modules/facebookModule/resources/icons/facebook.jpg",
+                color: "#3b5b96"
+            },
+            text: "poly.js is the a collection of AMD modules that shim (aka 'polyfill') old browsers to support modern (aka 'ES5-ish') javascript. poly.js is unique amongst ES5-ish shims because it: - is modular, not monolithic - is tiny - is configurable to suit your code - can be minified using a has-aware optimizer"
+        },
+        {
+            id: 6,
+            type: {
+                id: "When",
+                icon: "source/modules/twitterModule/resources/icons/twitter.png",
+                color: "#3CBAFF"
+            },
+            text: "When.js is cujojs's lightweight CommonJS Promises/A and when() implementation, derived from the async core of wire.js, cujojs's IOC Container. It also provides several other useful Promise-related concepts, such as joining multiple promises, mapping and reducing collections of promises, timed promises, and has a robust unit test suite."
+        },
+        {
+            id: 7,
+            type: {
+                id: "Meld",
+                icon: "source/modules/twitterModule/resources/icons/twitter.png",
+                color: "#3CBAFF"
+            },
+            text: "Aspect Oriented Programming for Javascript. It allows you to change the behavior of, or add behavior to methods and functions (including constructors) non-invasively."
         }
     ]
 });
